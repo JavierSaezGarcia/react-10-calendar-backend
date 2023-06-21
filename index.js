769dac1c2ddb74d9ec8850bc96286180248f59ae
javiersaezgarcia <banbleac@gmail.com>
2023-06-21 18:21:04 +0200

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
-
+const PORT = process.env.PORT || 3000;
 
 // console.log( process.env.PORT );
 
@@ -32,6 +32,6 @@ app.use('/api/events', require('./routes/events'));
 
 
 // escuchar peticiones
-app.listen(process.env.PORT,  () => {
-    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+app.listen(PORT,  () => {
+    console.log(`Servidor corriendo en puerto ${ PORT }`);
 });
