7254ab334e6dedf58c1a33bd41b6e6426572fafe
javiersaezgarcia <banbleac@gmail.com>
2023-06-22 13:43:34 +0200

    update uid name

--- controllers/auth.js
+++ controllers/auth.js
@@ -99,7 +99,9 @@ const revalidarToken = async(req, res = response) => {
     const token = await generarJWT(uid, name);
 
     res.json({
-        ok: true,       
+        ok: true, 
+        uid,
+        name,      
         token
     })
 }
