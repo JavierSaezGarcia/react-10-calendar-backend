8f4585a324bd80f2d4c66e306332048d0cc7c7d4
javiersaezgarcia <banbleac@gmail.com>
2023-06-28 12:37:55 +0200

    update last

--- index.js
+++ index.js
@@ -12,11 +12,14 @@ const app = express();
 // Base de datos
 dbConnection();
 
+const path = require('path'); // Esto al principio, junto al resto de importaciones
+
 // Cors configuracion básica
 app.use(cors());
 
 // Directorio publico
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Lectura y parseo del body "middleware"
 app.use(express.json());
@@ -30,12 +33,20 @@ app.use('/api/auth', require('./routes/auth'));
 // TODO: CRUD Eventos CRUD
 app.use('/api/events', require('./routes/events'));
 
-app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+// app.get('*', (req, res) => {
+//     res.sendFile(__dirname + '/public/index.html');
+// });
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
 
 // escuchar peticiones
 app.listen(PORT,  () => {
     console.log(`Servidor corriendo en puerto ${ PORT }`);
 });
+
+
+
+ 
+
+ 
