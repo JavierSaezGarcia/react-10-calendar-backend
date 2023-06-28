5a518f36803bc60d1805f45bfbe5fde43407a714
javiersaezgarcia <banbleac@gmail.com>
2023-06-28 13:32:48 +0200

    update public

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ const app = express();
 // Base de datos
 dbConnection();
 
-const path = require('path'); // Esto al principio, junto al resto de importaciones
+// const path = require('path'); // Esto al principio, junto al resto de importaciones
 
 // Cors configuracion básica
 app.use(cors());
 
 // Directorio publico
-// app.use(express.static('public'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Lectura y parseo del body "middleware"
 app.use(express.json());
@@ -33,12 +33,12 @@ app.use('/api/auth', require('./routes/auth'));
 // TODO: CRUD Eventos CRUD
 app.use('/api/events', require('./routes/events'));
 
-// app.get('*', (req, res) => {
-//     res.sendFile(__dirname + '/public/index.html');
-// });
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
 });
+// app.get('/*', function(req,res) {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
 // escuchar peticiones
 app.listen(PORT,  () => {
